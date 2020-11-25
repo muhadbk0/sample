88371f52909db73be133a3b8553de4225b629f04
dwith geeth <dwithgeeth@gmail.com>
2020-11-25 11:34:24 -0800

    sample

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Head from 'next/head'
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      …
+    </>
+  )
+}
